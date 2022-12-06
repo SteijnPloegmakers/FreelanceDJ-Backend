7967075e69aba115ee47ae796d1ce07df2b4f02a
Steijn Ploegmakers <461991@student.fontys.nl>
2022-12-06 18:18:08 +0100

    made app solid and deleted warnings

--- FreelanceDJ/Models/DjAccount/AddDjAccount.cs
+++ FreelanceDJ/Models/DjAccount/AddDjAccount.cs
@@ -2,10 +2,10 @@
 {
     public class AddDjAccount
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Email { get; set; }
-        public long Phone { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public long Phone { get; set; } 
         public int Price { get; set; }
     }
 }
